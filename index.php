df2089ff47c82e0028cd421ff92ad97fceca1eb6
Bryan Karaffa <bryankaraffa@gmail.com>
2019-01-21 12:27:18 -0800

    Add Matomo

--- index.php
+++ index.php
@@ -242,6 +242,21 @@ preg_match_all('/(?:Water Temperature:<\/strong> )(\d*.\d*)(?:&#176;)([CF])(?: \
 
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+  <!-- Matomo -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//matomo.calcoasttech.com/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', '2']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <!-- End Matomo Code -->	  
   </head>
   <!-- Content -->
   <body>
